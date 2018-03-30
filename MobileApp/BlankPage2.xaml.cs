e92d785864cc83cc2304fd7592199ddfd071968b
Cathal Ryan <CathalRyan96>
2018-03-30 14:18:31 +0100

    Added button and another blank page

--- MobileApp/BlankPage2.xaml.cs
+++ MobileApp/BlankPage2.xaml.cs
@@ -20,16 +20,11 @@ namespace MobileApp
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class BlankPage1 : Page
+    public sealed partial class BlankPage2 : Page
     {
-        public BlankPage1()
+        public BlankPage2()
         {
             this.InitializeComponent();
-        }
-
-        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
         }
     }
 }
