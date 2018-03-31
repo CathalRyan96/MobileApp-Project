43ab06764edcf0d79c68a4ba6f5beba25d2fe84c
Cathal Ryan <CathalRyan96>
2018-03-31 13:44:12 +0100

    added another page

--- MobileApp/BlankPage3.xaml.cs
+++ MobileApp/BlankPage3.xaml.cs
@@ -20,21 +20,11 @@ namespace MobileApp
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class BlankPage1 : Page
+    public sealed partial class BlankPage3 : Page
     {
-        public BlankPage1()
+        public BlankPage3()
         {
             this.InitializeComponent();
-        }
-
-        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void button_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
